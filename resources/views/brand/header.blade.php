084ae5b04517b978a351bf4fc9bcaa832bfb65be
Miguel <azurduy.campos.josemiguel@gmail.com>
2022-10-02 20:37:07 -0400

    primer commit

---
+++ resources/views/brand/header.blade.php
@@ -0,0 +1,17 @@
+@push('head')
+    <link
+        href="/favicon.ico"
+        id="favicon"
+        rel="icon"
+    >
+@endpush
+
+<p class="h2 n-m font-thin v-center">
+    <x-orchid-icon path="feed"/>
+
+    <span class="m-l d-none d-sm-block">
+        INSUCONS
+        <small class="v-top opacity">ERP</small>
+    </span>
+</p>
+ 
